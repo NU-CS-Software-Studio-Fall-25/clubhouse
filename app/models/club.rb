35e5c24451cab49ec2422b51ba526f1bbf4dca9c
AlexShen21 <121585614+AlexShen21@users.noreply.github.com>
2025-12-07 13:53:54 -0600

    added profile picture uploads for clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -4,6 +4,7 @@ class Club < ApplicationRecord
     has_many :memberships, dependent: :destroy
     has_many :members, through: :memberships, source: :user
     has_many :chat_messages, dependent: :destroy
+    has_one_attached :profile_photo
 
     validates :name, presence: true
 end
